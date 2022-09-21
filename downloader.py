05b1603107c66ce7419ecef2b8b9173677aac672
REficul-dRAw <110767236+REficul-dRAw@users.noreply.github.com>
2022-09-21 20:47:32 +0200

    Html Downloader
    Module for downloading html

---
+++ downloader.py
@@ -0,0 +1,28 @@
+
+import requests
+
+FAKE_AGENT = True
+
+if FAKE_AGENT:
+    try:
+        from fake_useragent import UserAgent
+    except ImportError:
+        FAKE_AGENT = False
+        print("Lib fake_useragent not installed.\n If you want to fake the useragent than: pip install fake_useragent")
+
+def getFakeHeader():
+    return {"User-Agent" : UserAgent().random}        
+
+def getHtml(sUrl):
+    if not FAKE_AGENT:
+        oResponse = requests.get(sUrl)
+    else:
+        oResponse = requests.get(sUrl, headers=getFakeHeader())
+        
+    return oResponse.content if oResponse.status_code == 200 else None
+        
+#region tests
+# print(getHtml("https://ident.me"))
+# print(getHtml("https://httpstat.us/403"))
+#endregion
+    
